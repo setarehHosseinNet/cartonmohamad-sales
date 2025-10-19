b1fd24424935dfc236a946b2cf21da70a2d8c669
Administrator <Administrator@SERVER-14-AUTOM>
2025-10-19 14:32:42 +0330

    Add project files.

---
+++ cartonmohamad-sales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cartonmohamad_sales.Startup))]
+namespace cartonmohamad_sales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
